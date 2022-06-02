06760e655d32554a5fd2bd4e52d89e454a5c2980
Emre Can Satık <emrecanstk@gmail.com>
2022-06-02 21:06:15 +0300

    methods are filled

--- gameManagement/src/gameManagement/OfferManager.java
+++ gameManagement/src/gameManagement/OfferManager.java
@@ -1,16 +1,22 @@
 package gameManagement;
 
-public class OfferManager implements GameInterface {
+public class OfferManager {
 
-	@Override
-	public void add(Game game) {
-		// TODO Auto-generated method stub
-		
+	public void add(Game game, Offer offer) {
+		System.out.println(game.name+" is now %"+offer.discount+" off.");
+		game.price = game.price - game.price*(offer.discount/100);
+		game.hasDiscount = offer.discount;
 	}
 
-	@Override
-	public void delete(Game game) {
-		// TODO Auto-generated method stub
+	public void delete(Game game, Offer offer) {
+		if(game.hasDiscount == offer.discount) {
+			System.out.println("%"+offer.discount+" on "+game.name+" has been deleted.");
+			game.price = game.price + game.price*(offer.discount/100);
+			game.hasDiscount = 0;
+		}
+		else {
+			System.out.println(game.name+" does not have this discount.");
+		}
 		
 	}
 
