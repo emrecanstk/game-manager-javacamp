1d9e75806393ceb81fb2a074927f5b8cebd486d0
Emre Can Satık <emrecanstk@gmail.com>
2022-06-02 20:25:21 +0300

    loggers list set

--- gameManagement/src/gameManagement/Main.java
+++ gameManagement/src/gameManagement/Main.java
@@ -3,7 +3,8 @@ package gameManagement;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		LoggerInterface[] loggers = {new SmsLogger(), new EmailLogger(), new DataBaseLogger()};
 
 	}
 
--- gameManagement/src/gameManagement/Utils.java
+++ gameManagement/src/gameManagement/Utils.java
@@ -1,5 +1,11 @@
 package gameManagement;
 
 public class Utils {
-
+	
+	public static void runLoggers(LoggerInterface[] loggers, String message) {
+		for(LoggerInterface logger: loggers) {
+			logger.log(message);
+		}
+	}
+	
 }
