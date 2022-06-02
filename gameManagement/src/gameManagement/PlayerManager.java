1a19cba85f3745d97b50efdd51dc8b88962ded8c
Emre Can Satık <emrecanstk@gmail.com>
2022-06-02 20:43:12 +0300

    added methods

--- gameManagement/src/gameManagement/PlayerManager.java
+++ gameManagement/src/gameManagement/PlayerManager.java
@@ -1,5 +1,17 @@
 package gameManagement;
 
 public class PlayerManager {
-
+	private LoggerInterface[] loggers;
+	
+	public PlayerManager(LoggerInterface[] loggers) {
+		this.loggers = loggers;
+	}
+	
+	public void save(Player player) {
+		Utils.runLoggers(loggers, player.getFirstName());
+	}
+	
+	public void delete(Player player) {
+		
+	}
 }
