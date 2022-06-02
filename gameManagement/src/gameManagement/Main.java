b975413405be5ce268f1b504ab888256eede328c
Emre Can Satık <emrecanstk@gmail.com>
2022-06-02 21:08:12 +0300

    Main updated

--- gameManagement/src/gameManagement/Main.java
+++ gameManagement/src/gameManagement/Main.java
@@ -5,7 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 
 		LoggerInterface[] loggers = {new SmsLogger(), new EmailLogger(), new DataBaseLogger()};
+		
 		PlayerManager playerManager = new PlayerManager(loggers);
+		GameManager gameManager = new GameManager();
+		OfferManager offerManager = new OfferManager();
 		
 		Player emre = new Player(1, "Emre Can", "Satık", "emrecanwick", 1000120, 2002);
 		Player gulnur = new Player(2, "Gülnur", "Vırdınlı", "gvv", 1003500, 2001);
@@ -13,20 +16,20 @@ public class Main {
 		Player melike = new Player(4, "Melike", "Satık", "melike155", 1780000, 1978);
 		Player aycan = new Player(5, "Aycan", "Satık", "Ican", 1000650, 1998);
 		
-		Player[] players = {emre,gulnur,ercan,melike,aycan};
+		Player[] players = {};
 		
 		Game gtaSa = new Game(1,"Grand Theft Auto: San Andreas","Rockstar Games",35);
 		Game mbw = new Game(2,"Mount and Blade: Warband","TaleWorlds",84);
 		Game gtaV = new Game(3, "Grand Theft Auto: V", "Rockstar Games", 210);
 		Game lol = new Game(4, "Leauge of Legends", "Riot Gmes", 0);
 		
-		Game[] games = {gtaSa,mbw,gtaV,lol};
+		Game[] games = {};
 		
 		Offer of1 = new Offer(1, 40);
 		Offer of2 = new Offer(2, 15);
 		Offer of3 = new Offer(3, 20);
 		
-		Offer[] offers = {of1,of2,of3};
+		Offer[] offers = {};
 	}
 
 }
